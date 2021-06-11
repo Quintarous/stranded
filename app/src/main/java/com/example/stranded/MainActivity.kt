b8273e3a86d24301a8a13f16c784664cc01dc662
Austin Green <austinoguy51@gmail.com>
2021-06-11 15:05:08 -0600

    Animation test
    Added an animation test image view to the main activity.

--- app/src/main/java/com/example/stranded/MainActivity.kt
+++ app/src/main/java/com/example/stranded/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.stranded
 
-import android.media.MediaPlayer
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.ImageView
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var testAnimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_chat_page)
 
         //creating and registering the notification testing channel
         //DELETE ONCE DONE TESTING
         createChannel(this, "test", "Test Channel")
+
+        val testImage = findViewById<ImageView>(R.id.testAnimation).apply {
+            setBackgroundResource(R.drawable.test_animation)
+            testAnimation = background as AnimationDrawable
+        }
+
+        testImage.setOnClickListener { testAnimation.start() }
     }
 }
